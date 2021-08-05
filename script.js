df0521888112b10138fb21ca05fc1cced0dbe31e
FDVleuten <86839267+FDVleuten@users.noreply.github.com>
2021-08-05 12:26:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//  leeftijd
+const age = 99;
+
+if (age >= 18) {
+
+  console.log("Top, jij mag naar binnen");
+} else {
+  console.log("helaas, je mag niet naar binnen");
+}
+
+// dit is voor vrouw of man
+const isFemal = false;
+
+if (isFemal) {
+    console.log ("Top, jij bent inderdaad een vrouw");
+} else { 
+    console.log ("Jij bent geen vrouw");
+}
+
+// wel of geen Bob
+const driverStatus = "Bob";
+
+if (driverStatus == "Bob") {
+    console.log ("jij mag rijden");
+} else {
+    console.log ("jij mag niet rijden");
+}
